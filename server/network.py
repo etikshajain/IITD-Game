221f3a6240f2e0770d4f3332b76edd36c9c8036b
Adit Malhotra <aditmalhotra8@gmail.com>
2024-04-17 17:10:12 +0530

    added server code

--- server/network.py
+++ server/network.py
@@ -1,12 +1,13 @@
 import socket
 import pickle
 import pygame
+from config.server import *
 
 class Network:
   def __init__(self):
     self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.server = "127.0.0.1"
-    self.port = 8080
+    self.server = IP_ADDR
+    self.port = PORT
     self.addr = (self.server, self.port)
     self.p = self.connect()
 
@@ -16,13 +17,13 @@ class Network:
   def connect(self):
     try:
       self.client.connect(self.addr)
-      return pickle.loads(self.client.recv(4096))
+      return pickle.loads(self.client.recv(BUFFER_SIZE))
     except:
       print("Error")
 
   def send(self, data):
     try:
       self.client.send(pickle.dumps(data))
-      return pickle.loads(self.client.recv(4096))
+      return pickle.loads(self.client.recv(BUFFER_SIZE))
     except socket.error as e:
       print(e)
